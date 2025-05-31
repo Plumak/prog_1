7a3a1510d4e5cbe393356843a0792d248f578fc1
Plumak <115593719+Plumak@users.noreply.github.com>
2025-05-31 14:11:57 -0300

    Delete secuencia_b_esta_en_arreglo_A.java

--- secuencia_b_esta_en_arreglo_A.java
+++
@@ -1,8 +0,0 @@
-public static boolean secuencia_b_esta_en_arregloA(int [] arregloA,int [] arregloB, int inicio, int fin, int inicioB, int finB){
-if (fin-inicio+1 == finB-inicioB+1){ 
-while (inicio<= fin && inicioB<= finB && arregloA[inicio] == arregloB[inicioB]){
-inicio++;
-inicioB++;
-}}
-return inicio > fin;
-}
